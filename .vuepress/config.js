3874ae6d9ee2d2fe84a1df194b8bd976170862b5
zuijiaoshangyang2 <1940536383@qq.com>
2023-08-19 11:08:36 +0800

    change

---
+++ .vuepress/config.js
@@ -0,0 +1,118 @@
+const { reverse, sortBy } = require('lodash')
+
+module.exports = {
+    title: '云匿博客',
+    base: '/',
+    description: '记录一些有用但基础的玩机指南',
+    theme: require.resolve('../../'),
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/myicon.png`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#2c3e50'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: '/myicon.png'
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/myicon.png',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/rodo.png'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }],
+        ['meta', {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1.0'
+        }]
+    ],
+    themeConfig: {
+        logo: '/avatars/rodber.png',
+        pwa: false,
+        authors: [
+            {
+                name: 'yuniee',
+                avatar: '/avatars/mylogo.png',
+                link: 'https://www.yuniee.de/',
+                linktext: 'Card',
+            },
+            {
+                name: 'rodber',
+                avatar: '/avatars/rodber.png',
+                link: 'https://github.com/rodber',
+                linktext: 'GitHub',
+            },
+            {
+                name: 'Sape',
+                avatar: '/avatars/sape.jpg',
+                link: 'https://rodolfoberrios.com/',
+                linktext: 'EL RODO SAPEE!',
+            },
+            {
+                name: 'OMA',
+                avatar: '/avatars/oma.jpg',
+                link: 'https://chevereto.com/',
+                linktext: 'Chevereto.com',
+            }
+        ],
+        footer: {
+            contact: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/rodber',
+                },
+                {
+                    type: 'mail',
+                    link: 'mailto:mail@yuniee.de',
+                },
+                {
+                    type: 'globe',
+                    link: 'https://yuniee.de',
+                },
+            ],
+            copyright: [
+                // {
+                //   text: '',
+                //   link: '',
+                // },
+            ],
+        },
+        sitemap: {
+            hostname: 'https://www.yuniee.de/'
+        },
+        comment: {
+          service: 'disqus',
+          shortname: 'demowebsite',
+        },
+        // newsletter: {
+        //   endpoint: '/'
+        // },
+        feed: {
+            canonical_base: 'https://www.yuniee.de/',
+            sort: (entries) => reverse(sortBy(entries, 'date')),
+        },
+        smoothScroll: true
+    },
+}
