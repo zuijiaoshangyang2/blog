ecf0e7390d69a089a80bd526e98ddf68e3c7536f
yuniee <72388093+zuijiaoshangyang2@users.noreply.github.com>
2023-08-19 18:02:10 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -47,10 +47,6 @@ module.exports = {
             name: 'viewport',
             content: 'width=device-width, initial-scale=1.0'
         }]
-        ['meta', {
-            name: 'google-site-verification',
-            content: 'yqDuatsyT_e7mhC7IH0QNT-8cm1Y2UH1xLQM6C5qzAI'
-        }]
     ],
     themeConfig: {
         logo: '/avatars/mylogo.png',
