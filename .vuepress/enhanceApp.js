c1c9c9eee11853593fe4b704f2030895ebd8fa6a
yuniee <72388093+zuijiaoshangyang2@users.noreply.github.com>
2023-08-20 13:13:21 +0800

    Update enhanceApp.js

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,23 +1,15 @@
-/**
- * Client app enhancement file.
- *
- * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
- */
-
 export default ({
   Vue,
   options,
   router,
   siteData
 }) => {
-  // Create a <script> element
-  const scriptElement = document.createElement('script');
-  scriptElement.defer = true;
-  scriptElement.dataset.domain = 'tj.naaa.top';
-  scriptElement.src = 'https://tj.naaa.top/js/script.js';
+  // 添加自定义脚本标签
+  const script = document.createElement('script');
+  script.defer = true;
+  script.dataset.domain = 'tj.naaa.top';
+  script.src = 'https://tj.naaa.top/js/script.js';
+  document.head.appendChild(script);
 
-  // Append the <script> element to the <head> tag
-  document.head.appendChild(scriptElement);
-
-  // ...apply other enhancements to the app if needed
+  // ...应用其他增强逻辑
 }
