aed7926dc34d53493fc986b24458a73df8f72710
yuniee <72388093+zuijiaoshangyang2@users.noreply.github.com>
2023-11-15 23:06:26 +0800

    Update enhanceApp.js

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,14 +1,20 @@
-/**
- * Client app enhancement file.
- *
- * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
- */
-
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData // site metadata
+  Vue,
+  options,
+  router,
+  siteData
 }) => {
-  // ...apply enhancements to the app
+  if (typeof window !== 'undefined') {
+    import('vue-google-adsense')
+      .then(module => {
+        const Ads = module.default
+        Vue.use(require('vue-script2'))
+        Vue.use(Ads.Adsense)
+        Vue.use(Ads.InArticleAdsense)
+        Vue.use(Ads.InFeedAdsense)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 }
